c90b7162e45cc229cb635638f0c72826672f97ec
Anisha <anisha.0612@live.com>
2021-02-17 16:48:49 -0500

    enviroment varibale add to config vars

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,7 @@ import Clarifai from "clarifai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = new Clarifai.App({ apiKey: "1ec91d93e3b64c76b1bdc3bf6327a73f" });
+const app = new Clarifai.App({ apiKey: process.env.CLARIFAI_KEY });
 
 const handleApiCall = (req, res) => {
   app.models
