7a985608d20d64904bbc0bcaef4c2bf96aad9d0c
Anisha <anisha.0612@live.com>
2021-02-17 15:38:57 -0500

    remove env files

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,7 @@ import Clarifai from "clarifai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = new Clarifai.App({ apikey: process.env.REACT_APP_API_KEY });
+const app = new Clarifai.App({ apiKey: "1ec91d93e3b64c76b1bdc3bf6327a73f" });
 
 const handleApiCall = (req, res) => {
   app.models
